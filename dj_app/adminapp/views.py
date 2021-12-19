f5f9e4e97f806df1554d83ea175069fa37614fb0
testdriver <ssmerkalov3@gmail.com>
2021-12-20 00:31:53 +0300

    add Lesson16

--- dj_app/adminapp/views.py
+++ dj_app/adminapp/views.py
@@ -16,6 +16,9 @@ from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from django.views.generic.edit import DeleteView
 from django.views.generic.detail import DetailView
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+from django.db import connection
 
 
 
@@ -276,3 +279,17 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = 'adminapp/product_read.html' 
 
+def db_profile_by_type(prefix, type, queries):
+   update_queries = list(filter(lambda x: type in x['sql'], queries))
+   print(f'db_profile {type} for {prefix}:')
+   [print(query['sql']) for query in update_queries]
+
+@receiver(pre_save, sender=ProductCategory)
+def product_is_active_update_productcategory_save(sender, instance, **kwargs):
+   if instance.pk:
+       if instance.is_active:
+           instance.product_set.update(is_active=True)
+       else:
+           instance.product_set.update(is_active=False)
+
+       db_profile_by_type(sender, 'UPDATE', connection.queries)
--- dj_app/basketapp/views.py
+++ dj_app/basketapp/views.py
@@ -14,7 +14,11 @@ def basket_add(request, pk):
     product = get_object_or_404(Product, pk=pk)
     
     basket = Basket.objects.filter(user=request.user, product=product).first()
-
+        old_basket_item = Basket.get_product(user=request.user, product=product)
+   if old_basket_item:
+       old_basket_item[0].quantity += 1
+       old_basket_item[0].save()
+   else:
     if not basket:
         basket = Basket(user=request.user, product=product)
 
